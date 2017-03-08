c5567ddc170bd93692e6d276af96c4c3fa39f3e2
zacboz <zacboz@gmail.com>
2017-03-08 16:20:41 -0700

    finished 5 kata 8 problems

--- 8kyu/8kata.js
+++ 8kyu/8kata.js
@@ -46,3 +46,92 @@ function seatsInTheater(nCols, nRows, col, row) {
 function getChar(c){
  return String.fromCharCode(c);
 }
+
+// 6. Remove String Spaces
+function noSpace(x){
+  return x.replace(/\s+/g, '')
+}
+
+// 7. Heads and Legs
+function animals(heads, legs){
+  if (heads > 1000 || legs > 1000){
+    return "No solutions";
+  } else if(legs > heads * 4){
+    return "No solutions";
+  }else if (heads > 0 && legs > 0){
+    var double = heads * 2;
+    var twiceCows = legs - double;
+    var cows = twiceCows/2;
+    var chickens = heads - cows;
+    if (chickens < 0 || cows < 0){
+      return "No solutions";
+    } else if (chickens % 1 == 0 && cows % 1 == 0){
+      return [chickens, cows];
+    } else {
+      return "No solutions"
+    }
+  } else if (heads === 0 && legs === 0) {
+    return [0, 0];
+  } else if (heads <= 0 || legs <= 0) {
+    return "No solutions";
+  }
+}
+                // a better solution:
+                function animals(heads, legs){
+                  if (heads == 0 && legs == 0)
+                    return [0, 0];
+
+                  if (heads < 0 || legs < 0)
+                    return 'No solutions';
+
+                  var c = legs/2 - heads;
+                  var h = heads - c;
+
+                  if (h < 0 || c < 0 || h%1 > 0 || c%1 > 0)
+                    return 'No solutions';
+
+                  return [h, c];
+                }
+
+// 8. Sum without highest and lowest number
+function sumArray(array) {
+  if (array == null) {
+    return 0;
+  } else if (array.length < 2) {
+    return 0;
+  } else {
+    array = array.sort(function(a, b) {return a - b;});
+    var total = 0;
+    for (var i = 1; i < array.length - 1; i++) {
+        total += array[i];
+    }
+    return total;
+  }
+}
+
+// 9. Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string
+function remove(s){
+  s = s.replace(/!+/g, '');
+  return s + '!';
+}
+
+// 10. Invert values
+function invert(array) {
+  var inverted = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === 0){
+      inverted.push(array[i] = 0);
+    } else if(array[i] > 0){
+      inverted.push(-array[i]);
+    } else if (array[i] < 0) {
+      inverted.push(Math.abs(array[i]));
+    }
+  }
+  return inverted;
+}
+                  // a better solutions
+                  function invert(array) {
+                     return array.map( x => x === 0 ? x : -x);
+                  }
+
+// 11. 
