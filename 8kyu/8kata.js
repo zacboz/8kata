33f024f500399b6de8d0f6eb93a61ff571df5562
zacboz <zacboz@gmail.com>
2017-03-24 16:07:08 -0600

    finsihed toy problem

--- 8kyu/8kata.js
+++ 8kyu/8kata.js
@@ -271,3 +271,13 @@ function greet(name){
 function dutyFree(normPrice, discount, hol){
   return Math.floor(hol/(normPrice *(discount/100)));
 }
+
+// .25 Are they opposite?
+// sometimes its easier to establish what is false and have everything else be true
+function isOpposite(s1,s2){
+  if(s1 === s2 || s1.toLowerCase() !== s2.toLowerCase()) return false; //
+  for(var i = 0; i < s1.length; i++) {
+    if(s1.charAt(i) === s2.charAt(i)) return false;
+  }
+  return true;
+}
